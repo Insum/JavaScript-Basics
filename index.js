c95ef815508926881895583a7db191ac162c3df3
Eldar Terzic <eldar.terzic@insum.se>
2023-01-05 15:44:24 +0100

    function

--- index.js
+++ index.js
@@ -9,7 +9,17 @@ console.log(person);
 
 
 let selectedColors = ['red', 'blue'];
+selectedColors[2] = 'green'
 
 console.log(selectedColors);
 
 console.log(selectedColors[1]);
+
+
+
+function square(number){
+    return number * number;
+}
+
+var result = square(2);
+console.log(result);
